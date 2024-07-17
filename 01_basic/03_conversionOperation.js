26c1159f5562e0c33b6e6d532b9f7529089fd4ef
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2024-07-17 09:56:04 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "laml"
+
+// console.log(typeof (score))
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+let someMummber = 33
+let stringNumber = String(someMummber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
