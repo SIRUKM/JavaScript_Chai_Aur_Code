9d6c57c4020c996859c8998298f6a1f22466a860
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2024-07-17 10:14:45 +0000

    Operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -16,5 +16,32 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someMummber = 33
 let stringNumber = String(someMummber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// *********************** Operations ******************************************
+
+let value = 3
+let negativeValue = - value
+// console.log(negativeValue)
+
+
+let str1 = 'kamlesh'
+let str2 = 'mahto'
+// console.log(str1 + str2)
+
+// console.log('1' + 2)
+// console.log('1'+ 2 + 2)
+// console.log(1 + 2 + '2')
+
+
+// console.log(+true)
+// console.log(true)
+
+let num1 , num2, num3
+num1 = num2 = num3 = 2 +2 //bahi ye sub mat karna
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
