54539f3338319a964a313609c24b2bf0099bd449
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2024-07-17 10:25:39 +0000

    Comparison

---
+++ 01_basic/04_comparsion.js
@@ -0,0 +1,11 @@
+// console.log('2'>1) //javascript converts '2' to 2
+
+console.log(null>0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log('2' == 2)
+
+// === value and datatype
+console.log('2' == 2) //false
