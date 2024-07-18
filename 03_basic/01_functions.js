508474c5f7210a8517a7289fe41b91e96f464a97
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2024-07-18 11:52:30 +0000

    function

---
+++ 03_basic/01_functions.js
@@ -0,0 +1,30 @@
+function sayMyName(){
+    console.log("hello")
+}
+
+// sayMyName()
+
+
+// function addTwoNumbers(num1 , num2){
+//     console.log(num1 + num2)
+// }
+
+// const result = addTwoNumbers(2,3) //return hoga tub he variable me store hoga
+// console.log("Result:", result); 
+
+
+function addTwoNumbers(num1 , num2){
+    let result = num1 + num2
+    return 
+    console.log("hell0") // ye line kabhi v print nahi hoga, return ke baad neekla jata hai 
+}
+
+const result = addTwoNumbers(2,3) //return hoga tub he variable me store hoga
+// console.log("Result:", result); 
+
+
+function loginUserMessage(username){
+    return `${username} jsut logged in`
+}
+
+console.log(loginUserMessage("hitesh"))
