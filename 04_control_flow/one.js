011ad75f8ab1f508e88c39f18222f522b645b146
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2024-07-19 13:12:41 +0530

    feat: Add control flow examples and truthy/falsy values

---
+++ 04_control_flow/one.js
@@ -0,0 +1,36 @@
+// const isUserLoggedIn = true;
+// const temperature = 20;
+
+// if(2 == '2') {
+//   console.log('2 == "2"');
+// }
+
+// const score = 200;
+// if (score > 100) {
+//   let power = "fly";
+//   console.log(`User Poweer : ${power}`);
+// }
+
+// *************** Switch Case ****************
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log('January');
+//         break;
+//     case 2:
+//         console.log('February');
+//         break;
+//     case 3:
+//         console.log('March');
+//         break;
+//     default:
+//         console.log('Invalid Month');
+//         break;
+// }
+
+
+// ********* Ternianry Operator **********
+
+const iceCream = 'chocolate';
+iceCream === 'chocolate' ? console.log('Yay, I love chocolate ice cream!') : console.log('Aww, but chocolate is my favorite...');
---
+++ 04_control_flow/truthy_falsy.js
@@ -0,0 +1,17 @@
+const userEmail = "hdfdjf@gmail.com";
+if (userEmail) {// isme koi bhi string hoga toh true hoga aisa maan liya jaata hai
+  console.log("Email is valid");
+} else {
+  console.log("Email is not valid");
+}
+
+
+
+
+// falsey values in JavaScript
+// false, 0, -0, BigInt(0n), "", null, undefined, NaN
+
+// truthy values in JavaScript
+
+"0", "false", " ", [], {}, function() {}
+
